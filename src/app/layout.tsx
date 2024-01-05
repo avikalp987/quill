ff0095f73b8b48b5b1696d806ceebe2efc2fa6e4
vikalp <avikalp233@gmail.com>
2024-01-05 13:49:03 +0530

    Creating the navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { cn } from '@/lib/utils'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,7 +21,10 @@ export default function RootLayout({
       <body className={cn(
         "min-h-screen font-sans antialiased grainy",
         inter.className
-      )}>{children}</body>
+      )}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
