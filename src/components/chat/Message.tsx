fc60b0649a611c1f1335c0478ffd197afa4a1430
vikalp <avikalp233@gmail.com>
2024-01-12 17:03:28 +0530

    Infinite queries for performence

---
+++ src/components/chat/Message.tsx
@@ -0,0 +1,7 @@
+const Message = () => {
+    return ( 
+        <div></div>
+     );
+}
+ 
+export default Message;
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -123,11 +123,53 @@ export const appRouter = router({
     limit: z.number().min(1).max(100).nullish(),
     cursor: z.string().nullish(),
     fileId: z.string()
-  })).query(({input, ctx}) => {
+  })).query(async ({input, ctx}) => {
     const {userId} = ctx
     const {fileId, cursor} = input
 
     const limit = input.limit ?? INFINITE_QUERY_LIMIT
+
+    const file = await db.file.findFirst({
+      where: {
+        id: fileId,
+        userId,
+      }
+    })
+
+    if(!file)
+    {
+      throw new TRPCError({code: "NOT_FOUND"})
+    }
+
+    const messages = await db.message.findMany({
+      take: limit+1,
+      where: {
+        fileId,
+      },
+      orderBy: {
+        createdAt: "desc"
+      },
+      cursor: cursor ? {id: cursor} : undefined,
+      select: {
+        id: true,
+        isUserMessage: true,
+        createdAt: true,
+        text: true
+      }
+    })
+
+    let nextCursor: typeof cursor | undefined = undefined
+    if(messages.length>limit)
+    {
+      const nextItem = messages.pop()
+      nextCursor = nextItem?.id
+    }
+
+    return {
+      messages,
+      nextCursor,
+    }
+
   }),
 });
 
