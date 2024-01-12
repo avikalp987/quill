0d54e1dc7b6ef542d88eab4ba5185b715029bbf3
vikalp <avikalp233@gmail.com>
2024-01-12 08:29:54 +0530

    Streaming api response in real time

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,5 +1,12 @@
+import { db } from "@/db";
+import { openai } from "@/lib/openai";
+import { pinecone } from "@/lib/pinecone";
+import { SendMessageValidator } from "@/lib/validators/SendMessageValidator";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { PineconeStore } from "@langchain/community/vectorstores/pinecone";
+import { OpenAIEmbeddings } from "@langchain/openai";
 import { NextRequest } from "next/server";
+import { OpenAIStream, StreamingTextResponse } from "ai"
 
 export const POST = async (
     req: NextRequest
@@ -19,4 +26,109 @@ export const POST = async (
 
     const {id: userId} = user
 
+    //making sure that the type of data we want exists
+    const {fileId, message} = SendMessageValidator.parse(body)
+
+    //finding the file from our database
+    const file = await db.file.findFirst({
+        where: {
+            id: fileId,
+            userId,
+        }
+    })
+
+    //if no such file is present
+    if(!file)
+    {
+        return new Response("Not found", {status: 404})
+    }
+
+    //if the file is present add the message to that file
+    await db.message.create({
+        data: {
+            text: message,
+            isUserMessage: true,
+            userId,
+            fileId,
+        }
+    })
+
+
+    // vectorize the message
+    const embeddings = new OpenAIEmbeddings({
+        openAIApiKey: process.env.OPENAI_API_KEY
+    })
+
+    const pineconeIndex = pinecone.Index("quill")
+
+    const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+        pineconeIndex,
+        namespace: file.id,
+    })
+
+    const results = await vectorStore.similaritySearch(message, 4)
+
+    const prevMessages = await db.message.findMany({
+        where: {
+            fileId,
+        },
+        orderBy: {
+            createdAt: "asc"
+        },
+        take: 6,
+    })
+
+    const formattedPrevMessages = prevMessages.map((msg) => ({
+        role: msg.isUserMessage ? "user" as const : "assistant" as const,
+        content: msg.text
+    }))
+
+    const response = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        temperature: 0,
+        stream: true,
+        messages: [
+          {
+            role: 'system',
+            content:
+              'Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format.',
+          },
+          {
+            role: 'user',
+            content: `Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format. \nIf you don't know the answer, just say that you don't know, don't try to make up an answer.
+            
+      \n----------------\n
+      
+      PREVIOUS CONVERSATION:
+      ${formattedPrevMessages.map((message) => {
+        if (message.role === 'user')
+          return `User: ${message.content}\n`
+        return `Assistant: ${message.content}\n`
+      })}
+      
+      \n----------------\n
+      
+      CONTEXT:
+      ${results.map((r) => r.pageContent).join('\n\n')}
+      
+      USER INPUT: ${message}`,
+          },
+        ],
+      })
+    
+      const stream = OpenAIStream(response, {
+        async onCompletion(completion) {
+          await db.message.create({
+            data: {
+              text: completion,
+              isUserMessage: false,
+              fileId,
+              userId,
+            },
+          })
+        },
+      })
+    
+      return new StreamingTextResponse(stream)
+    
 }
--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,6 +1,11 @@
 import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+import {PDFLoader} from "langchain/document_loaders/fs/pdf"
+import { OpenAIEmbeddings } from "@langchain/openai"
+import {PineconeStore} from "@langchain/community/vectorstores/pinecone"
+import { pinecone } from "@/lib/pinecone";
  
 const f = createUploadthing();
  
@@ -31,6 +36,52 @@ export const ourFileRouter = {
                 uploadStatus: "PROCESSING",
             }
         })
+
+        //
+        try {
+            //get the file
+            const response = await fetch (file.url)
+
+            const blob = await response.blob()
+
+            const loader = new PDFLoader(blob)
+
+            const pageLevelDocs = await loader.load()
+
+            const pagesAmt = pageLevelDocs.length
+
+            //vectorize and index the entire document
+            const pineconeIndex = pinecone.Index("quill")
+
+            const embeddings = new OpenAIEmbeddings({
+                openAIApiKey: process.env.OPENAI_API_KEY
+            })
+
+            await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+                pineconeIndex,
+                namespace: createdFile.id,
+            })
+
+
+            await db.file.update({
+                data: {
+                    uploadStatus: "SUCCESS"
+                },
+                where: {
+                    id: createdFile.id
+                }
+            })
+
+        } catch (error: any) {
+            await db.file.update({
+                data: {
+                    uploadStatus: "FAILED"
+                },
+                where: {
+                    id: createdFile.id
+                }
+            })
+        }
     }),
 } satisfies FileRouter;
  
